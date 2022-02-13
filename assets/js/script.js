11caaf564b51b6076b23807c1b928407e5ffab0e
Brittany Fortenberry <95709599+bfortenberry22@users.noreply.github.com>
2022-02-12 22:16:54 -0600

    ability to edit task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 //universal variables go here
 var dateEl = document.querySelector("#currentDay");
+var descriptionEl = document.querySelector(".description");
 
 //function to add date
 var getDate = function(){
@@ -8,8 +9,29 @@ var getDate = function(){
 };
 getDate();
 
-//create a table
 
-//edit event 
+//edit event
+$(".row").on("click", "p", function(event){
+    // get current text of p element
+    var text = $(this)
+        .text()
+        .trim();
+    // replace element with a new textarea
+    $(".button").on("click", function(event){
+        var textInput = $("<textarea>").val(text)
+        $(this).replaceWith(textInput);
+    })
+});
+
+//color code
+var colorTime = function(){
+    var now = moment(now).get('hour');
+    if (9<now){
+        $("#hnine").addClass("past");
+    }
+}
+colorTime();
+
+
 
 //store in local storage
