0441833178bfe84826b912d2512dcc56e54bd22b
Brittany Fortenberry <95709599+bfortenberry22@users.noreply.github.com>
2022-02-13 20:56:12 -0600

    color code complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,18 +17,87 @@ $(".row").on("click", "p", function(event){
         .text()
         .trim();
     // replace element with a new textarea
-    $(".button").on("click", function(event){
-        var textInput = $("<textarea>").val(text)
-        $(this).replaceWith(textInput);
-    })
+    var textInput = $("<textarea>").val(text)
+    $(this).replaceWith(textInput);
 });
 
 //color code
 var colorTime = function(){
     var now = moment(now).get('hour');
-    if (9<now){
-        $("#hnine").addClass("past");
+    console.log(now);
+    if (9 == now){
+        $("#hNine").removeClass("future");
+        $("#hNine").addClass("present");
     }
+    if (9<now){
+        $("#hNine").removeClass("future");
+        $("#hNine").addClass("past");
+    }
+    if (10 == now){
+        $("#hNine").removeClass("future");
+        $("#hNine").addClass("present");
+    }
+    if (10<now){
+        $("#hTen").removeClass("future");
+        $("#hTen").addClass("past");
+    }
+    if (11 == now){
+        $("#hEleven").removeClass("future");
+        $("#hEleven").addClass("present");
+    }
+    if (11<now){
+        $("#hEleven").removeClass("future");
+        $("#hEleven").addClass("past");
+    }
+    if (12 == now){
+        $("#hTwelve").removeClass("future");
+        $("#hTwelve").addClass("present");
+    }
+    if (12<now){
+        $("#hTwelve").removeClass("future");
+        $("#hTwelve").addClass("past");
+    }
+    if (13 == now){
+        $("#hOne").removeClass("future");
+        $("#hOne").addClass("present");
+    }
+    if (13<now){
+        $("#hOne").removeClass("future");
+        $("#hOne").addClass("past");
+    }
+    if (14 == now){
+        $("#hTwo").removeClass("future");
+        $("#hTwo").addClass("present");
+    }
+    if (14<now){
+        $("#hTwo").removeClass("future");
+        $("#hTwo").addClass("past");
+    }
+    if (15 == now){
+        $("#hThree").removeClass("future");
+        $("#hThree").addClass("present");
+    }
+    if (15<now){
+        $("#hThree").removeClass("future");
+        $("#hThree").addClass("past");
+    }
+    if (16 == now){
+        $("#hFour").removeClass("future");
+        $("#hFour").addClass("present");
+    }
+    if (16<now){
+        $("#hFour").removeClass("future");
+        $("#hFour").addClass("past");
+    }
+    if (17 == now){
+        $("#hFive").removeClass("future");
+        $("#hFive").addClass("present");
+    }
+    if (17<now){
+        $("#hFive").removeClass("future");
+        $("#hFive").addClass("past");
+    }
+    
 }
 colorTime();
 
